d2ff9b1c9a742c86b08cc58325961cd04d13226d
Suroj Maharjan <surojmaharjan70@gmail.com>
2024-09-11 18:06:21 +0545

    Next Change

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
+from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='home'),
 ]
